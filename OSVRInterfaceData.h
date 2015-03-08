e36137dea251c6c1b0d61a732e39eb7730fa54a3
Ryan Pavlik <ryan@sensics.com>
2015-03-07 18:08:42 -0600

    Apply license header.

--- OSVRInterfaceData.h
+++ OSVRInterfaceData.h
@@ -10,10 +10,17 @@
 
 // Copyright 2014 Sensics, Inc.
 //
-// All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// (Final version intended to be licensed under
-// the Apache License, Version 2.0)
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef INCLUDED_OSVRInterfaceData_h_GUID_20F69D2D_747D_4D90_688E_B5F286B788ED
 #define INCLUDED_OSVRInterfaceData_h_GUID_20F69D2D_747D_4D90_688E_B5F286B788ED
